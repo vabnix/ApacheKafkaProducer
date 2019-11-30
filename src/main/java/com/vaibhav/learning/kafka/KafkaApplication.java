3b31566c29aedcf5adea86978ddb5985aeb503bb
Vaibhav Srivastava <vsrivastava@lwolf.com>
2019-11-29 23:35:09 -0600

    Producing Message with and without keys

--- src/main/java/com/vaibhav/learning/kafka/KafkaApplication.java
+++ src/main/java/com/vaibhav/learning/kafka/KafkaApplication.java
@@ -1,18 +1,20 @@
 package com.vaibhav.learning.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class KafkaApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
         String bootstrapServer = "127.0.0.1:9092";
 
         //Creating Producer Properties
@@ -21,17 +23,73 @@ public class KafkaApplication {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+
+        CallProducer(properties);
+        CallProducerWithKey(properties);
+
+
+
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+    private static void CallProducer(Properties properties) {
+        Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
         //Creating Producer
         KafkaProducer kafkaProducer = new KafkaProducer(properties);
-        ProducerRecord<String, String> record = new ProducerRecord<>("first_spring_topic", "Hello Vaibhav");
-        //Sending data
-        kafkaProducer.send(record);
 
+        for (int i = 0; i < 10; i++){
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_spring_topic", "Hello Vaibhav " + i);
+            //Sending data
+            kafkaProducer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if(exception == null){
+                        logger.info(metadata.timestamp() + " Received new Metadata " +
+                                " Topic: " + metadata.topic()  +
+                                " Partition " + metadata.partition() +
+                                " Has Offset " + metadata.hasOffset() +
+                                " Offset " + metadata.offset());
+                    } else {
+                        logger.error("Error while producing", exception);
+                    }
+                }
+            });
+        }
         //flush data
         kafkaProducer.flush();
         kafkaProducer.close();
+    }
 
-        SpringApplication.run(KafkaApplication.class, args);
+
+    private static void CallProducerWithKey(Properties properties) throws ExecutionException, InterruptedException {
+        Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+
+        for (int i = 0; i < 10; i++){
+            String topic = "first_spring_topic";
+            String value = "Hello Vaibhav " + i;
+            String key = "id_"+1;
+
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic,key, value);
+            //Sending data
+            kafkaProducer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if(exception == null){
+                        logger.info(metadata.timestamp() + " Received new Metadata " +
+                                " Topic: " + metadata.topic()  +
+                                " Partition " + metadata.partition() +
+                                " Has Offset " + metadata.hasOffset() +
+                                " Offset " + metadata.offset());
+                    } else {
+                        logger.error("Error while producing", exception);
+                    }
+                }
+            }).get();
+        }
+        //flush data
+        kafkaProducer.flush();
+        kafkaProducer.close();
     }
 
 }
